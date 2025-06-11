4b9119320bd7a39fc30e5487fd9f8b5e8755616e
Harshit Singh <139043409+harshitneverdebugs@users.noreply.github.com>
2025-06-11 19:37:39 +0530

    CI/Cd 11

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -17,25 +17,25 @@ logging.basicConfig(level=logging.INFO)
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
 # Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+dagshub_token = os.getenv("CAPSTONE_TEST")
+if not dagshub_token:
+    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "vikashdas770"
-# repo_name = "YT-Capstone-Project"
+dagshub_url = "https://dagshub.com"
+repo_owner = "harshitneverdebugs"
+repo_name = "testing"
 
-# # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 # -------------------------------------------------------------------------------------
 
 # Below code block is for local use
 # -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri('https://dagshub.com/harshitneverdebugs/testing.mlflow')
-dagshub.init(repo_owner='harshitneverdebugs', repo_name='testing', mlflow=True)
+# mlflow.set_tracking_uri('https://dagshub.com/harshitneverdebugs/testing.mlflow')
+# dagshub.init(repo_owner='harshitneverdebugs', repo_name='testing', mlflow=True)
 # -------------------------------------------------------------------------------------
 
 def load_model(file_path: str):
